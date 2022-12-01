cbe6612ef44603843c538bb126eb5a1920350e2f
Darren Pak <dapak@chapman.edu>
2022-11-30 18:30:02 -0800

    Added sort

--- diskscheduler.cpp
+++ diskscheduler.cpp
@@ -7,12 +7,17 @@ using namespace std;
 
 int INITIAL_POS;
 int fcfs(int *queue, int size);
+int* sort(int *queue, int size);
 
 int main(int argc, char *argv[]) {
-    INITIAL_POS = 5;
-    int array[5] = {10,5,2,4,7};
-    int fcfs_count = fcfs(array,INITIAL_POS);
-    cout << fcfs_count << endl;
+    INITIAL_POS = 53;
+    int arrSize = 8;
+    int req[8] = {98, 183, 37, 122, 14, 124, 65, 67};
+    int* req2;
+    req2 = sort(req,arrSize);
+    for (int i = 0; i < arrSize; i++) {
+        cout << req[i] << endl;
+    }
 }
 
 int fcfs(int *queue, int size) {
@@ -23,4 +28,16 @@ int fcfs(int *queue, int size) {
         position = queue[i];
     }
     return result;
+}
+
+int* sort(int *queue, int size) {
+    for (int i = 0; i < (size - 1); i++) {
+        if (queue[i] > queue[i + 1]) {
+            int hold = queue[i];
+            queue[i] = queue[i+1];
+            queue[i+1] = hold;
+            i = 0;
+        }
+    }
+    return queue;
 }
